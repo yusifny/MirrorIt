b71e30448cda4e75f02be395b640700ab0b23fae
Yusif Nazarbayov (CodeAcademy) <99797435+yusifny@users.noreply.github.com>
2022-06-11 23:06:12 +0400

    uploaded second time

---
+++ MirrorIt/MirrorIt/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using static System.Console;
+namespace MirrorIt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string item = ReadLine();
+            WriteLine(MirrorIt(item));
+        }
+        
+        static string MirrorIt(string item)
+        {
+            string candidate = "";
+            string mirroredOne = "";
+            for (int i = item.Length - 1; i >= 0; i--)
+            {
+                candidate += item[i];
+                mirroredOne = candidate;
+            }
+            return mirroredOne;
+        }
+    }
+}
